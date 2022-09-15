56992f95e11ae203f129c120a149b8eb7af9fab7
kimnahee <knh6851@naver.com>
2022-09-15 17:49:12 +0900

    타임리프 레이아웃 추가/main 화면 대충 구성(사이드바/헤더)/스터디게시판 view 파일 생성

--- ITrip/src/main/java/co/itrip/prj/ITripApplication.java
+++ ITrip/src/main/java/co/itrip/prj/ITripApplication.java
@@ -2,12 +2,24 @@ package co.itrip.prj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class ITripApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ITripApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String main() {
+		return "main/main";
+	}
+	
+	@GetMapping("/study.do")
+	public String study() {
+		return "community/study";
+	}
 }
