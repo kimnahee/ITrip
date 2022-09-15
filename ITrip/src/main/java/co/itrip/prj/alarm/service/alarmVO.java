80b103926497e118718cd77bbc6e5bccee2a8a24
zmszmswl <terrango115@naver.com>
2022-09-15 16:39:32 +0900

    알람, 팔로우 VO

---
+++ ITrip/src/main/java/co/itrip/prj/alarm/service/alarmVO.java
@@ -0,0 +1,17 @@
+package co.itrip.prj.alarm.service;
+
+import lombok.Data;
+
+@Data
+public class alarmVO {
+	
+	// 알람테이블
+	private int alarmNo; // 알람 순번 
+	private String guideId; // 가이드 아이디 (강사)
+	private String memberId; // 유저 아이디 (학생)
+	private int classNo; // 클래스 번호
+	private String alarmChk; // 알람 확인여부
+	private String alarmMsg; // 알람내용
+	
+
+}
