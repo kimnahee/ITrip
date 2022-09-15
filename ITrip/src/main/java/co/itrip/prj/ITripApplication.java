cc5b8fef61a26d38bc829e61369a6b22789a9882
soo <wjddk2131@naver.com>
2022-09-15 14:04:20 +0900

    1

---
+++ ITrip/src/main/java/co/itrip/prj/ITripApplication.java
@@ -0,0 +1,13 @@
+package co.itrip.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITripApplication.class, args);
+	}
+
+}
---
+++ ITrip/src/test/java/co/itrip/prj/ITripApplicationTests.java
@@ -0,0 +1,13 @@
+package co.itrip.prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
