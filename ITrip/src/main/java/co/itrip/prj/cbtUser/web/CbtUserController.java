ee0c2c4ce47484130e7d0435e9b650b853b4869f
KyungA <KyungA@localhost>
2022-09-18 23:44:01 +0900

    20220918

--- ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
+++ ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
@@ -3,6 +3,7 @@ package co.itrip.prj.cbtUser.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.itrip.prj.cbtUser.mapper.CbtUserMapper;
@@ -16,10 +17,12 @@ public class CbtUserController {
 	
 	@RequestMapping("/cbtUserList")
 	public String cbtUserList(CbtUserVO vo, Model model) {
-		System.out.println(vo);
-		System.out.println(vo.getQues());
-		System.out.println(vo.getLangCd());
 		model.addAttribute("cbtList",dao.cbtUserList(vo));
 		return "cbtUser/cbtUserList";
+	}
+	
+	@GetMapping("/cbtUserInsertForm")
+	public String cbtUserInsertForm() {
+		return "cbtUser/cbtUserInsertForm";
 	}
 }
