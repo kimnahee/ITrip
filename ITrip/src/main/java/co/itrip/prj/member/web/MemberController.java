912332743bfa44e44e7c05c351548107274ecdbe
zmszmswl <terrango115@naver.com>
2022-09-19 18:03:28 +0900

    가이드 마이페이지, 데이터피커

--- ITrip/src/main/java/co/itrip/prj/member/web/MemberController.java
+++ ITrip/src/main/java/co/itrip/prj/member/web/MemberController.java
@@ -1,11 +1,13 @@
 package co.itrip.prj.member.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import co.itrip.prj.member.community.service.CommunityService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import co.itrip.prj.community.service.CommunityService;
 
 @Controller
 public class MemberController {
@@ -57,4 +59,30 @@ public class MemberController {
 	public String cStart(Model model) {
 		return "member/cstart";
 	}
+	
+	// 가이드 마이페이지
+	@GetMapping("/gmyPage.do")
+	public String gmyPage() {
+		return "member/gmypage";
+	}
+	
+	// 가이드 마이페이지 가이드가 개설한 컨설턴트
+	@GetMapping("/gconsult.do")
+	public String gconsult() {
+		return "member/gconsult";
+	}
+	
+	// 가이드 마이페이지 가이드가 개설한 클래스
+	@GetMapping("/gclass.do")
+	public String gclass() {
+		return "member/gclass";
+	}
+	
+	// 가이드 마이페이지 가이드가 개설한 클래스
+	@RequestMapping("/grevice.do")
+	public String grevice(HttpServletRequest request) {
+		request.getSession().setAttribute("id", "eunji");
+		return "member/grevice";
+	}
+	
 }
