1ad71d0d69fef45b688d5c3fcb37f4a255e5ef9d
zmszmswl <terrango115@naver.com>
2022-09-20 18:06:44 +0900

    머지머지

---
+++ ITrip/src/main/java/co/itrip/prj/cmmncd/service/CmmnCdServiceImpl.java
@@ -0,0 +1,26 @@
+package co.itrip.prj.cmmncd.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.itrip.prj.cmmncd.mapper.CmmnCdMapper;
+
+public class CmmnCdServiceImpl implements CmmnCdService {
+
+	@Autowired
+	private CmmnCdMapper map; // 공통코드 맵퍼
+	
+	@Override
+	public List<CmmnCdVO> careerCdList() {
+		// 경력 공통코드 리스트
+		return map.careerCdList();
+	}
+
+	@Override
+	public List<CmmnCdVO> dutyCdList() {
+		// 직무 공통코드 리스트
+		return map.dutyCdList();
+	}
+
+}
