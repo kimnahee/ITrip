229c1792c65355ad360ab919d139e9b3662ccb9a
admin <admin@DESKTOP-BJ6C3LP>
2022-09-20 18:05:14 +0900

    cbt list 채점화면 구현중

--- ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
+++ ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
@@ -18,19 +18,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import co.itrip.prj.cbtUser.mapper.CbtUserMapper;
+import co.itrip.prj.cbtUser.service.CbtUserService;
 import co.itrip.prj.cbtUser.service.CbtUserVO;
 import co.itrip.prj.langcd.mapper.LangCdMapper;
+import co.itrip.prj.langcd.service.LangCdService;
 import co.itrip.prj.utpcd.mapper.UtpCdMapper;
+import co.itrip.prj.utpcd.service.UtpCdService;
 
 @Controller
 public class CbtUserController {
 	
 	@Autowired
-	CbtUserMapper cuDao;
+	CbtUserService cuDao;
 	@Autowired
-	UtpCdMapper utpDao;
+	UtpCdService utpDao;
 	@Autowired
-	LangCdMapper langDao;
+	LangCdService langDao;
 	
 	int r=0;
 	
@@ -49,26 +52,17 @@ public class CbtUserController {
 	}
 	
 	@PostMapping("/cbtUserInsert.do")
-	public String cbtUserInsert(CbtUserVO vo, Model  model, @RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
-		
-		String saveFolder ="c://fileUpload";//저장할 공간 폴더 명
-		
-		String originalFileName = file.getOriginalFilename();//넘어온 파일명
-		
-		//if(!originalFileName.isEmpty()) {
-		if(!file.isEmpty()) {
-			//파일명 충돌방지를 위한 파일별명만듦
-			String saveFileName = UUID.randomUUID().toString() + originalFileName.substring(originalFileName.lastIndexOf('.'));
-			//파일을 물리적 위치에 저장
-			file.transferTo(new File(saveFolder, saveFileName));
-			
-			vo.setAttach(originalFileName);
-			vo.setAttachDir(saveFolder + File.separator + saveFileName);
-			
-		}
+	public String cbtUserInsert(CbtUserVO vo, Model  model, MultipartFile file) throws IllegalStateException, IOException {
+		String projectPath = System.getProperty("user.dir")+"/src/main/resources/static/files"; //프로젝트 경로
+		UUID uuid = UUID.randomUUID();
+		String filename = uuid + "_" + file.getOriginalFilename();
+		File saveFile = new File(projectPath, filename);
+		file.transferTo(saveFile);
+		vo.setAttach(filename);
+		String path = "/files/" + filename;
+		vo.setAttachDir(path);
 		cuDao.cbtUserInsert(vo);
 	
-		//model.addAttribute("r", r);
 		model.addAttribute("langCdList",langDao.langCdList());
 		return "cbtUser/cbtUserMain";
 		//return "cbtUserMain.do";
