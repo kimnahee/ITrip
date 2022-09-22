256a3dad1b35b1b5d805a63b768c0732ca82ae3f
admin <admin@DESKTOP-BJ6C3LP>
2022-09-22 18:05:00 +0900

    유저 cbt (객관식,OX)채점구현, 문제에 대한 풀이 및 댓글구현중

---
+++ ITrip/src/main/java/co/itrip/prj/cbtUser/web/AjaxCbtUserController.java
@@ -0,0 +1,21 @@
+package co.itrip.prj.cbtUser.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.itrip.prj.cbtUser.service.CbtUserService;
+import co.itrip.prj.cbtUser.service.CbtUserVO;
+
+@RestController
+public class AjaxCbtUserController {
+
+	@Autowired
+	CbtUserService dao;
+	
+	@RequestMapping("/ajaxQuestion.do")
+	public CbtUserVO AjaxQuestion(CbtUserVO vo,Model model) {
+		return dao.ajaxQuestion(vo);
+	}
+}
