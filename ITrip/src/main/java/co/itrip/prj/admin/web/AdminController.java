1e964a33abb731344fad832a3fed41e10254ad48
soo <wjddk2131@naver.com>
2022-09-26 18:06:17 +0900

    1234

--- ITrip/src/main/java/co/itrip/prj/admin/web/AdminController.java
+++ ITrip/src/main/java/co/itrip/prj/admin/web/AdminController.java
@@ -1,15 +1,41 @@
 package co.itrip.prj.admin.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import co.itrip.prj.consult.service.ConsultService;
+import co.itrip.prj.consult.service.ConsultVO;
 
 @Controller
 public class AdminController {
 	
-	@GetMapping("/approve.do")
-	public String approve() {
+	@Autowired
+	private ConsultService conService; // 1:1 상담서비스
+	
+	
+	@GetMapping("/appClass.do")
+	public String appClass() {
 		
-		return "admin/approve";
+		return "admin/appclass";
 	}
+	
+	@GetMapping("/appConsult.do")
+	public String appConsult(ConsultVO vo, Model model, HttpServletRequest request,
+			@RequestParam(required = false, defaultValue = "1") int pageNum,
+			@RequestParam(required = false, defaultValue = "8") int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		model.addAttribute("pageInfo", PageInfo.of(conService.findAll(vo)));
+		return "admin/appconsult";
+	}
+	
+	
 
 }
