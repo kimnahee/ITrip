7d125a6cdc5102f4c40af34e9934759221fab8fe
KyungA <KyungA@172.30.1.40>
2022-10-03 21:12:05 +0900

    myCbtUser문제단건조회,수정,삭제구현/오답노트다시풀고 정답,풀이띄우기구현

--- ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
+++ ITrip/src/main/java/co/itrip/prj/cbtUser/web/CbtUserController.java
@@ -2,6 +2,8 @@ package co.itrip.prj.cbtUser.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,13 +23,16 @@ import org.springframework.web.multipart.MultipartFile;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+import co.itrip.prj.cbtGuide.service.CbtGuideVO;
 import co.itrip.prj.cbtUser.service.CbtUserService;
 import co.itrip.prj.cbtUser.service.CbtUserVO;
+import co.itrip.prj.cmmncd.service.CmmnCdService;
 import co.itrip.prj.feedback.service.FeedbackService;
 import co.itrip.prj.feedback.service.FeedbackVO;
 import co.itrip.prj.langcd.service.LangCdService;
 import co.itrip.prj.member.service.MemberVO;
 import co.itrip.prj.utpcd.service.UtpCdService;
+import oracle.jdbc.proxy.annotation.Post;
 
 @Controller
 public class CbtUserController {
@@ -38,6 +43,8 @@ public class CbtUserController {
 	UtpCdService utpDao;
 	@Autowired
 	LangCdService langDao;
+	@Autowired
+	CmmnCdService cmDao;
 	
 
 //	@RequestMapping("/cbtUserList.do")
@@ -91,5 +98,54 @@ public class CbtUserController {
 		model.addAttribute("langCdList",langDao.langCdList());
 		return "cbtUser/cbtUserMain";
 		//return "cbtUserMain.do";
+	}
+	
+
+	// 나의 문제 출제 목록 
+	@GetMapping("/cbtUserMyList.do")
+	public String cbtGuideMyList(Principal prin, CbtUserVO vo, Model model, HttpServletRequest request,
+			@RequestParam(required = false, defaultValue = "1") int pageNum,
+			@RequestParam(required = false, defaultValue = "10") int pageSize) {
+		
+		vo.setMemberId(prin.getName());
+		PageHelper.startPage(pageNum, pageSize); 
+		List<CbtUserVO> list = cuDao.cbtUserMyList(vo);
+	
+		model.addAttribute("pageInfo", PageInfo.of(list));
+		return "cbtUser/cbtUserMyList";
+	}
+	
+	// 나의 문제 상세보기
+	@PostMapping("/cbtUserMyOne.do")
+	public String cbtUserListOne(CbtUserVO vo, Model model) {
+		model.addAttribute("langCdList",langDao.langCdList());
+		String ucd = cmDao.cdNameList("U", vo.getUtpCd()); 
+	    String lcd = cmDao.cdNameList("L", vo.getLangCd());
+		vo.setUtpCdName(ucd);
+		vo.setLangCdName(lcd);
+		
+		model.addAttribute("myCbt", cuDao.cbtUserMyOne(vo));
+		return "cbtUser/cbtUserMyOne";
+	}
+	
+	@PostMapping("/cbtUserMyUpdate.do")
+	public String cbtUserMyUpdate(CbtUserVO vo, Model  model, MultipartFile file,HttpServletRequest request) throws IllegalStateException, IOException {
+		String saveFolder = ("");
+		String oFileName = file.getOriginalFilename();
+		if(!oFileName.isEmpty()) {
+			String sFileName = UUID.randomUUID().toString()+oFileName.substring(oFileName.lastIndexOf("."));
+			String path = fileDir+"/"+sFileName;
+			file.transferTo(new File(path));
+			vo.setAttach(oFileName); 
+			vo.setAttachDir(saveFolder+"/"+sFileName);
+		}
+		cuDao.cbtUserMyUpdate(vo);
+		return "redirect:cbtUserMyList.do";
+	}
+	
+	@PostMapping("/cbtUserMyDelete.do")
+	public String cbtUserMyDelete(CbtUserVO vo) {
+		cuDao.cbtUserMyDelete(vo);
+		return "redirect:cbtUserMyList.do";
 	}
 }
