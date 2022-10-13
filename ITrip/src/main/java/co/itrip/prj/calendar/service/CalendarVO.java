9eb3585d03b044ae57c4cb7ca630b7a3f3f4ebb2
knh <knh6851@naver.com>
2022-10-14 01:48:46 +0900

    캘린더 insert(수정필요함)

--- ITrip/src/main/java/co/itrip/prj/calendar/service/CalendarVO.java
+++ ITrip/src/main/java/co/itrip/prj/calendar/service/CalendarVO.java
@@ -12,7 +12,7 @@ import lombok.Data;
 public class CalendarVO {
 	
 	private int calendarNo; // 인공키
-	private String merchant_uid; // 외래키, 상담 결제 취소시 캘린더 리스트 삭제용도
+	private String merchantUid; // 외래키, 상담 결제 취소시 캘린더 리스트 삭제용도
 	
 	private String guideId; // 가이드 아이디
 	private String member_id; // 유저 아이디
@@ -20,7 +20,7 @@ public class CalendarVO {
 	private String name; // 제목
 	
 	//@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-	private Date conday; // 상담일
+	private String conday; // 상담일
 	//@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private String beginTime; // 시작시간
 	private int no; // 캘린더 번호
