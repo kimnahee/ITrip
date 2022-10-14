def2e64c5fcd961cea92d1a169621a140f9cb106
soo <wjddk2131@naver.com>
2022-10-14 17:58:26 +0900

    1234

--- ITrip/src/main/java/co/itrip/prj/guide/web/GuideController.java
+++ ITrip/src/main/java/co/itrip/prj/guide/web/GuideController.java
@@ -68,8 +68,7 @@ public class GuideController {
 		
 		String oFileName = file.getOriginalFilename();
 		if (!oFileName.isEmpty()) {
-			String sFileName = UUID.randomUUID().toString() + oFileName.substring(oFileName.lastIndexOf(".")); // 마지막.뒤에값
-																												// 가져오기
+			String sFileName = UUID.randomUUID().toString() + oFileName.substring(oFileName.lastIndexOf(".")); // 마지막.뒤에값 가져오기
 			String path = fileDir + "/guide/" + sFileName;
 			file.transferTo(new File(path));
 			vo.setAttach(oFileName);
