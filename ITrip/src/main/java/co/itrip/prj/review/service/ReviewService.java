995e1ec7033cf457e4e9c82a0fbba684759383d7
knh <knh6851@naver.com>
2022-10-17 00:44:50 +0900

    수정

--- ITrip/src/main/java/co/itrip/prj/review/service/ReviewService.java
+++ ITrip/src/main/java/co/itrip/prj/review/service/ReviewService.java
@@ -10,6 +10,7 @@ public interface ReviewService {
 	
 	int classReviewInsert(ReviewVO vo); // 소정 - 클래스 리뷰등록
 	ReviewVO classReviewSelect(ReviewVO vo);  // 소정 - 리뷰 단건조회
+	ReviewVO consultReviewSelect(ReviewVO vo);
 	int deleteReview(ReviewVO vo); //소정 - 리뷰삭제
 
 }
